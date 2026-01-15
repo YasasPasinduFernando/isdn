6b366fa59e93aac87fdfb891db39a62f91ffaa5f
YasasPasinduFernando <151065874+YasasPasinduFernando@users.noreply.github.com>
2026-01-15 10:49:58 +0530

    mobile accessable base url paths also added

--- config/config.php
+++ config/config.php
@@ -6,8 +6,16 @@ $isHttps = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off')
     || (isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == 443);
 $scheme = $isHttps ? 'https' : 'http';
 $host = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : 'localhost';
-$scriptDir = isset($_SERVER['SCRIPT_NAME']) ? rtrim(str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME'])), '/') : '';
-$basePath = $scriptDir === '/' ? '' : $scriptDir;
+
+$docRoot = isset($_SERVER['DOCUMENT_ROOT']) ? realpath($_SERVER['DOCUMENT_ROOT']) : false;
+$appRoot = realpath(__DIR__ . '/..');
+if ($docRoot && $appRoot && strpos($appRoot, $docRoot) === 0) {
+    $basePath = str_replace('\\', '/', substr($appRoot, strlen($docRoot)));
+} else {
+    $scriptDir = isset($_SERVER['SCRIPT_NAME']) ? dirname($_SERVER['SCRIPT_NAME']) : '';
+    $basePath = str_replace('\\', '/', $scriptDir);
+}
+$basePath = rtrim($basePath, '/');
 
 define('APP_URL', $scheme . '://' . $host . $basePath);
 define('BASE_PATH', $basePath);
